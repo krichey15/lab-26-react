72845dc2f1498864e6a110552f6d0f897c616eb2
Kyle Richey <richey.kyle.i@gmail.com>
2017-12-02 17:47:31 -0800

    random cows generated

--- lab-kyle/src/main.js
+++ lab-kyle/src/main.js
@@ -3,11 +3,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/main.scss';
-import {say} from 'Cowsay';
+import {say, DEFAULT, WWW, MUTILATED, DRAGON_AND_COW, HEAD_IN, SATANIC, SMALL, SURGERY, VADER} from 'Cowsay';
 import faker from 'faker';
 
 
 const main = document.getElementById('main');
+let cows = [DEFAULT, WWW, MUTILATED, DRAGON_AND_COW, HEAD_IN, SATANIC, SMALL, SURGERY, VADER];
 
 class Header extends React.Component {
   render() {
@@ -23,20 +24,28 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      content: 'woof'
+      content: say({
+        text: 'woof',
+        cow: DEFAULT})
     }
 
     this.generateMoo = this.generateMoo.bind(this);
   }
 
   generateMoo() {
-    this.setState({content: faker.hacker.phrase()});
+    this.setState({
+      content: say({
+        text:faker.hacker.phrase(),
+        cow: cows[Math.floor(Math.random() * cows.length)]
+      })
+    });
   }
+
   render(){
     return (
       <div>
         <Header />
-        <pre>{say({text: this.state.content})}</pre>
+        <pre>{this.state.content}</pre>
         <button onClick={this.generateMoo}>Click</button>
       </div>
     )
